e8dd4978e34aa45546694d3d8f2d5846fec39ee1
IvanDavydov <jacob.bobel1@gmail.com>
2026-05-30 14:49:04 +0500

    basic06 Реализована функция check_leap_year, проверяющая, является ли год високосным.

--- src/basic/basic06.js
+++ src/basic/basic06.js
@@ -3,6 +3,12 @@
 */
 
 export function check_leap_year(year) {
-  return false;
+  if (year % 400 === 0) {
+    return true;
+  }
+  if (year % 100 === 0) {
+    return false;
+  }
+  return year % 4 === 0;
 }
 
