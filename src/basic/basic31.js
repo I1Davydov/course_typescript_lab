4d0f7b2bbd57f2bfe142839229844c1e500f8fa0
IvanDavydov <jacob.bobel1@gmail.com>
2026-05-30 22:15:11 +0500

    basic31 Реализована функция transposeMatrix, которая транспонирует матрицу (меняет строки и столбцы местами).

--- src/basic/basic31.js
+++ src/basic/basic31.js
@@ -2,6 +2,26 @@
 	Реализуйте функцию transposeMatrix(matrix), которая транспонирует матрицу (меняет строки и столбцы местами).
 */
 
-export function transposeMatrix(matrix) {	
-	return [[]];
+export function transposeMatrix(matrix) {
+  if (matrix.length === 0) {
+    return [];
+  }
+  const cols = matrix[0].length;
+  if (cols === 0) {
+    return [];
+  }
+  const rows = matrix.length;
+  const result = [];
+  let j = 0;
+  while (j < cols) {
+    const newRow = [];
+    let i = 0;
+    while (i < rows) {
+      newRow.push(matrix[i][j]);
+      i++;
+    }
+    result.push(newRow);
+    j++;
+  }
+  return result;
 }
