1c16af7549954e98b93432586fcf4bbed7699ca4
IvanDavydov <jacob.bobel1@gmail.com>
2026-05-30 19:36:19 +0500

    basic24 Реализована функция replace_word, которая заменяет старое слово новым в предложении.

--- src/basic/basic24.js
+++ src/basic/basic24.js
@@ -3,6 +3,17 @@
 */
 
 export function replace_word(sentence, old_word, new_word) {
-    return "";
+  const words = sentence.split(' ');
+  let i = 0;
+  const result = [];
+  while (i < words.length) {
+    if (words[i] === old_word) {
+      result.push(new_word);
+    } else {
+      result.push(words[i]);
+    }
+    i++;
+  }
+  return result.join(' ');
 }
 
