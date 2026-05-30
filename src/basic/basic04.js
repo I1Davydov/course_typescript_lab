c11b85399917bc8f2b1d641c3c9856a4fc1d6554
IvanDavydov <jacob.bobel1@gmail.com>
2026-05-30 14:32:12 +0500

    basic04 Реализована функция calculate_discount, которая рассчитывает стоимость со скидкой и возвращает ошибку при скидке больше 100%.

--- src/basic/basic04.js
+++ src/basic/basic04.js
@@ -3,7 +3,10 @@
 */
 
 export function calculate_discount(price, discount) {
-  return "Ошибка! Процент скидки слишком большой.";
+  if (discount > 100) {
+    return "Ошибка! Процент скидки слишком большой.";
+  }
+  return price * (1 - discount / 100);
 }
 
 
