471dbb2d5ef302488fa354f42e39b469ff7a9667
IvanDavydov <jacob.bobel1@gmail.com>
2026-05-30 17:20:14 +0500

    basic17 Реализована функция remove_duplicates, которая удаляет дублирующиеся значения из массива, сохраняя порядок элементов, с использованием цикла while и метода includes.

--- src/basic/basic17.js
+++ src/basic/basic17.js
@@ -4,6 +4,13 @@
 */
 
 export function remove_duplicates(arr) {
-    const unique = [];
-    return unique;
+  const unique = [];
+  let i = 0;
+  while (i < arr.length) {
+    if (!unique.includes(arr[i])) {
+      unique.push(arr[i]);
+    }
+    i++;
+  }
+  return unique;
 }
