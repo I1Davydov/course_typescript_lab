1d2900f003674d4cd46903a985bd738b15826702
IvanDavydov <jacob.bobel1@gmail.com>
2026-05-30 14:39:10 +0500

    basic05 Реализована функция grade_student, которая возвращает оценку студента в зависимости от балла, используя конструкцию switch.

--- src/basic/basic05.js
+++ src/basic/basic05.js
@@ -8,7 +8,16 @@
 */
 
 export function grade_student(score) {
-  return 'неудовлетворительно'
+  switch (true) {
+    case (score >= 90):
+      return 'отлично';
+    case (score >= 75):
+      return 'хорошо';
+    case (score >= 60):
+      return 'удовлетворительно';
+    default:
+      return 'неудовлетворительно';
+  }
 }
 
 
