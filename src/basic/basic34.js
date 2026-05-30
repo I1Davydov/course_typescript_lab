f3d6bfb71b27f3c3490d4829c86314e0a03c57f6
IvanDavydov <jacob.bobel1@gmail.com>
2026-05-30 23:59:38 +0500

    basic34 Реализована функция isSymmetric, которая проверяет, симметрична ли матрица относительно главной диагонали.

--- src/basic/basic34.js
+++ src/basic/basic34.js
@@ -3,8 +3,26 @@
 */
 
 export function isSymmetric(matrix) {
-
+  const rows = matrix.length;
+  if (rows === 0) {
     return true;
+  }
+  const cols = matrix[0].length;
+  if (rows !== cols) {
+    return false;
+  }
+  let i = 0;
+  while (i < rows) {
+    let j = 0;
+    while (j < cols) {
+      if (matrix[i][j] !== matrix[j][i]) {
+        return false;
+      }
+      j++;
+    }
+    i++;
+  }
+  return true;
 }
 
 
