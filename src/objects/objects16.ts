ca903beacd4b793613ce8ec35207a18e53e27820
IvanDavydov <jacob.bobel1@gmail.com>
2026-05-31 16:35:17 +0500

    objects16 Реализована функция addToCollection, которая добавляет элемент в существующую коллекцию в объекте.

--- src/objects/objects16.ts
+++ src/objects/objects16.ts
@@ -2,11 +2,7 @@
 	Реализовать функцию addToCollection<T, K extends string>(obj: Record<K, T[]>, collectionName: K, element: T): Record<K, T[]>, которая добавляет элемент в существующую коллекцию в объекте.
 */
 
-export function addToCollection<T, K extends string>(
-    obj: Record<K, T[]>,
-    collectionName: K,
-    element: T
-): Record<K, T[]> {
-    return obj;
+export function addToCollection<T, K extends string>(obj: Record<K, T[]>, collectionName: K, element: T): Record<K, T[]> {
+	return { ...obj, [collectionName]: [...obj[collectionName], element] };
 }
 
