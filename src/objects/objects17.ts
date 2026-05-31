001bf855a36f156c35d69ffce9f0ce6d38d2047f
IvanDavydov <jacob.bobel1@gmail.com>
2026-05-31 16:44:49 +0500

    objects17 Реализована функция removeField, которая удаляет указанное поле из объекта, возвращая новый объект без этого поля и не изменяя исходный объект.

--- src/objects/objects17.ts
+++ src/objects/objects17.ts
@@ -2,7 +2,8 @@
 	Реализовать функцию removeField<T, K extends keyof T>(obj: T, field: K): Omit<T, K>, которая удаляет поле из объекта.
 */
 
-export function removeField<T, K extends keyof T>(obj: T, field: K): Omit<T, K> {    
-    return obj;
+export function removeField<T, K extends keyof T>(obj: T, field: K): Omit<T, K> {
+  const {[field]: _, ...rest} = obj;
+  return rest;
 }
 
