976adfda4c73b8f879cc68ddf2eb54c00e81c2d9
IvanDavydov <jacob.bobel1@gmail.com>
2026-06-01 21:11:32 +0500

    classes04 Реализован класс Point, который принимает координаты точки либо отдельно (по оси X и Y), либо сразу массивом координат [X,Y].

--- src/classes/classes04.ts
+++ src/classes/classes04.ts
@@ -7,6 +7,12 @@ export class Point {
     constructor(x: number, y: number);   // Конструктор с двумя числами
     constructor(coords: [number, number]);       // Конструктор с массивом чисел
     constructor(...args: any[]) {
-		
+        if (args.length === 1 && Array.isArray(args[0])) {
+            this.coords = [...args[0]];
+        } else if (args.length === 2 && typeof args[0] === 'number' && typeof args[1] === 'number') {
+            this.coords = [args[0], args[1]];
+        } else {
+            this.coords = [];
+        }
     }
 }
