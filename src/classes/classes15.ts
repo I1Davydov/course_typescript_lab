63cb537e54a8835ef700f00e0abbb5d33660f02d
IvanDavydov <jacob.bobel1@gmail.com>
2026-06-01 22:44:20 +0500

    classes15 Заменен статический метод info() на геттер в классе Student, который возвращает строку с текущим количеством студентов.

--- src/classes/classes15.ts
+++ src/classes/classes15.ts
@@ -12,7 +12,7 @@ export class Student {
 	this.last_name = last_name;
     Student.count++;
   }
-  static info(): string {
-    return `Всего ${Student.count} студентов`;
-  }
+   static get info(): string {
+     return `Всего ${Student.count} студентов`;
+   }
 }
