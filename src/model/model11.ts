7cf18b3b48f36766b32600b8b715a9c3d6f3a79a
IvanDavydov <jacob.bobel1@gmail.com>
2026-06-02 23:08:03 +0500

    model11 Реализованы оба абстрактных метода save и load в классе MemoryStorage для работы со строковыми данными.

--- src/model/model11.ts
+++ src/model/model11.ts
@@ -11,4 +11,11 @@ abstract class DataStorage {
 export class MemoryStorage extends DataStorage {
   private data: string = "";
   
+  save(data: string): void {
+    this.data = data;
+  }
+  
+  load(): string {
+    return this.data;
+  }
 }
