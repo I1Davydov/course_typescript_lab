c66fdb734f823f72c4c7a5ad05cc131a22864514
IvanDavydov <jacob.bobel1@gmail.com>
2026-06-02 13:34:04 +0500

    model07 Реализован класс Duck, который реализует оба интерфейса Swimmable и Diveable с методами swim() и dive(), выводящими соответствующие сообщения в консоль.

--- src/model/model07.ts
+++ src/model/model07.ts
@@ -11,5 +11,11 @@ interface Diveable {
 }
 
 export class Duck implements Swimmable, Diveable {
-
+  swim(): void {
+    console.log("Duck is swimming");
+  }
+  
+  dive(): void {
+    console.log("Duck is diving");
+  }
 }
