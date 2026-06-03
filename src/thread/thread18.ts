d81f3dbc29742285815b56cb62f8f0658e87df58
IvanDavydov <jacob.bobel1@gmail.com>
2026-06-04 00:38:45 +0500

    thread18 Реализована функция getUserWithFallback, которая сначала пытается получить пользователя через fetchUser, а при ошибке пробует получить данные через fetchUserData.

--- src/thread/thread18.ts
+++ src/thread/thread18.ts
@@ -5,5 +5,5 @@
 import { fetchUser, fetchUserData, type User } from "./promises";
 
 export function getUserWithFallback(id: number): Promise<User> {
-  // TODO: Реализовать fallback механизм
+  return fetchUser(id).catch(() => fetchUserData(id));
 }
