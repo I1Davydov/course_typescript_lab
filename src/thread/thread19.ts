029d466364ceddf7f70073b9b46ad6efeedeae29
IvanDavydov <jacob.bobel1@gmail.com>
2026-06-04 00:58:39 +0500

    thread19 Реализована функция fetchUserWithTimeout, которая отклоняет промис, если запрос занимает больше указанного времени (по умолчанию 100мс), с правильной очисткой таймаута.

--- src/thread/thread19.ts
+++ src/thread/thread19.ts
@@ -5,6 +5,20 @@
 import { fetchUser, type User } from "./promises";
 
 export function fetchUserWithTimeout(id: number, timeout = 100): Promise<User> {
-  // TODO: Добавить таймаут для промиса
-
+  let timeoutId: NodeJS.Timeout;
+  const promise = new Promise<User>((resolve, reject) => {
+    timeoutId = setTimeout(() => reject(new Error('Request timeout')), timeout);
+    
+    fetchUser(id)
+      .then(result => {
+        clearTimeout(timeoutId);
+        resolve(result);
+      })
+      .catch(error => {
+        clearTimeout(timeoutId);
+        reject(error);
+      });
+  });
+  
+  return promise;
 }
